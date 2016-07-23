092d75e3d759c79d4802a4203bf4c199e35f6932
M. Porooshani <porooshani@gmail.com>
2016-07-23 10:29:25 +0430

    Updated extension's options to public

--- Source/UIViewController+TransEasy.swift
+++ Source/UIViewController+TransEasy.swift
@@ -5,23 +5,54 @@
 //  Created by Mohammad Porooshani on 7/21/16.
 //  Copyright © 2016 Porooshani. All rights reserved.
 //
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 Mohammad Poroushani
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 import UIKit
 
 var PresentAssociatedHandler: UInt8 = 0
 
 public struct EasyTransPresentOptions {
-  var duration: NSTimeInterval = 0.4
-  let sourceView: UIView
-//  let destinationView: UIView
-  var blurStyle: UIBlurEffectStyle?
+  public var duration: NSTimeInterval = 0.4
+  public let sourceView: UIView
+  public let blurStyle: UIBlurEffectStyle?
+  
+  public init(duration: NSTimeInterval, sourceView: UIView, blurStyle: UIBlurEffectStyle? = nil) {
+    self.duration = duration
+    self.sourceView = sourceView
+    self.blurStyle = blurStyle
+  }
 }
 
 public struct EasyTransDismissOptions {
   
-  var duration: NSTimeInterval = 0.4
-//  let sourceView: UIView
-  let destinationView: UIView
+  public var duration: NSTimeInterval = 0.4
+  public let destinationView: UIView
+  
+  public init(duration: NSTimeInterval, destinationView: UIView) {
+    self.duration = duration
+    self.destinationView = destinationView
+  }
+  
 }
 
 public extension UIViewController {
